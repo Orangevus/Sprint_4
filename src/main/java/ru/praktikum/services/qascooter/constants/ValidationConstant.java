d55d80eb8ed2ce0994fcd92de59a934dfa8a63aa
Aleksandr Belimov <vontas@mail.ru>
2025-02-02 19:48:57 +0300

    Вторая попытка залить код в Git

--- src/main/java/ru/praktikum/services/qascooter/constants/ValidationConstant.java
+++ src/main/java/ru/praktikum/services/qascooter/constants/ValidationConstant.java
@@ -1,4 +1,11 @@
 package ru.praktikum.services.qascooter.constants;
 
 public class ValidationConstant {
+    public static final String INVALID_NAME = "abcde";
+    public static final String INVALID_ADDRESS = "abcde";
+    public static final String INVALID_PHONE = "8909";
+    public static final String VALID_FIRST_NAME = "Анна";
+    public static final String VALID_SECOND_NAME = "Коренина";
+    public static final String VALID_ADDRESS = "Москва";
+    public static final String VALID_PHONE = "89055552215";
 }
